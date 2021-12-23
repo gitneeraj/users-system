59c0eace9eb4ba5e3c5ba2bf71551f8c38bde0fa
Neeraj A <neeraj.a@LT360.local>
2021-12-23 20:01:15 +0530

    add basic routes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv  from "dotenv"
 
+import Routes from "./routes/index.js";
+
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 4000
 
+app.use('/api/v1', Routes)
 app.use('/', (_, res) => {
     return res.status(401).send('Unauthorized Access')
 })
